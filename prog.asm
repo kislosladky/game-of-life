16884835e3c0e57827c24624b9bd7fa4d9ee047b
kislosladky <126390826+kislosladky@users.noreply.github.com>
2023-04-21 21:57:28 +0700

    some progress

--- prog.asm
+++ prog.asm
@@ -1,12 +1,12 @@
 asect 0x00
 
 # ldi r0, default ## загружаю в память начальное положение клетки
-ldi r0, 0b00000100
-ld r0,r0
+ldi r0, 0b00000111
+# ld r0,r0
 ldi r1, cell_x
 st r1, r0
 ldi r1, cell_y
-st r1,r0
+st r1, r0   
 
 while 
     tst r0
@@ -14,12 +14,14 @@ stays pl
     push r0
     ldi r1, set ## в r1 лежит адрес входа от кнопки set
     ld r1, r2
+    clr r3
+    
     if
         tst r2
     is nz ## если set не 0, значит включен выбор состояния клетки
         push r2 ## кидаю в стек состояние
 
-        ldi r2, input ## загружаю данные от джойстика
+        ldi r2, joystick ## загружаю данные от джойстика
         ld r2, r2 
         
         shr r2 ## скидываю координату x в r3, правда, меняю местами
@@ -36,24 +38,29 @@ stays pl
         if
             tst r0
         is nz
-            push r0
             ldi r0, cell_y
             ld r0, r1
             inc r1 ## увеличиваем, чтобы сдвинуть клетку вниз
             st r0, r1
-            pop r0
+
+            ldi r0, changed
+            ldi r1, 1
+            st r0, r1
         else
             ldi r0, 0b00000001 
             and r2, r0
             if ## смотрю, не идем ли мы вверх
                 tst r0
             is nz 
-                push r0
+                
                 ldi r0, cell_y
                 ld r0, r1
                 dec r1 ## уменьшаем, чтобы сдвинуть клетку вверх
                 st r0, r1
-                pop r0
+                ldi r0, changed
+                ldi r1, 1
+                st r0, r1
+
             fi
         fi
 
@@ -66,99 +73,114 @@ stays pl
         if 
             tst r2
         is nz
-            push r0
             ldi r0, cell_x
             ld r0, r1
             dec r1 ## уменьшаем, чтобы сдвинуть клетку влево
             st r0, r1
-            pop r0
+
+            ldi r0, changed
+            ldi r1, 1
+            st r0, r1
         else
             ldi r0, 0b00000001 
             and r2, r0
             if
                 tst r0
             is nz
-                push r0
                 ldi r0, cell_x
                 ld r0, r1
                 inc r1 ## увеличиваем, чтобы сдвинуть клетку вправо
                 st r0, r1
-                pop r0
+
+                ldi r0, changed
+                ldi r1, 1
+                st r0, r1
             fi
         fi
 
 
-        ## теперь надо посмотреть, если нажаты всякие кнопки, чтобы загрузить новое значение в память
-        ## set уже 1, остается проверить alive и засунуть его значение в память
+        pop r2 ## возращаю состояние set
 
-        # ldi r0, alive
-        # ld r0, r0
-
-        # if
-        #     tst r0
-        # is nz
-            
-        # fi
-
-        
-        
-
-        pop r2
     else
+        ldi r3, changed
+        ld r3,r3
+        if
+            tst r3
+        is nz
+            ldi r0, 0
+            ldi r3, changed
+            st r3, r0 ## занулил changed, чтобы заново не менять клетку
 
-        ldi r0, cell_x
-        ld r0,r0
-        ldi r1, cell_y
-        ld r1,r1
-        shl r0
-        shl r0
-        shl r0
-        shl r0
 
-        shl r0
-        shl r1
-        shl r0
-        shl r1
-        shl r0
-        shl r1
-        shl r0
-        shl r1
+            ldi r0, cell_x
+            ld r0,r0
+            ldi r1, cell_y
+            ld r1,r1
+            shl r0
+            shl r0
+            shl r0
+            shl r0
 
-        ldi r0, addr
-        st r0, r1
+            shl r0
+            shl r1
+            shl r0
+            shl r1
+            shl r0
+            shl r1
+            shl r0
+            shl r1
+
+            ldi r0, addr
+            st r0, r1
+        fi
         # ldi r2, 
         ## надо аппаратно менять значение на противоположное, когда получаю новый адрес на выходе
-        ## может, для этого нужен новый флаг?????????????
+    
 
 
         ## потом надо бы сюда присобачить bcd вывод координат выбранной ячейки
     fi
 
-
+    clr r0
+    clr r1
+    clr r2
+    clr r3
     pop r0
 wend
 
 halt
 
-asect 0x80## мб стоит поменять потом
+# asect 0x80## мб стоит поменять потом
 
-# default: dc 0b00000100
-# default:
-asect 0x81
+asect 0x21           ## output
 # input: ds 1
-input: 
-asect 0x82
+joystick: 
+
+
+asect 0x22           ## output
 # set: ds 1
-set:
-asect 0x83
+set:    
+
+
+asect 0x23           ## not used
 # alive: ds 1
 alive:
-asect 0x84
-output:
-asect 0x85
+
+
+asect 0x24           ## is it used??
+changed:
+
+
+asect 0x25           ### input/output
 cell_x:
-asect 0x86
+
+
+asect 0x26           ### input/output
 cell_y:
-asect 0x87
+
+
+asect 0x27           ### output
 addr:
+
+
 end
